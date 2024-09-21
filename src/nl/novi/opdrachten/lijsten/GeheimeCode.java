e1e065de16284b87e3960bc98270de069c7e35dd
AnneKluytmans <annekluytmans1@gmail.com>
2024-09-21 14:06:04 +0200

    Finished GeheimeCode assignment

--- src/nl/novi/opdrachten/lijsten/GeheimeCode.java
+++ src/nl/novi/opdrachten/lijsten/GeheimeCode.java
@@ -2,6 +2,7 @@ package nl.novi.opdrachten.lijsten;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class GeheimeCode {
 
@@ -30,14 +31,72 @@ public class GeheimeCode {
         b) Als de positie in de lijst een even getal is, dan moet de cijfercombinatie omgedraaid worden.
          */
 
-        /*
-        Opdracht 3:
-        Schrijf een methode die de versleutelde lijst kan omzetten naar de ontsleutelde lijst.
-         */
+        List<String> encryptedMembers = encryptList(laResistanceMembers);
+        System.out.println("Encrypted list: ");
+        for (String member : encryptedMembers) {
+            System.out.println(member);
+        }
+        System.out.println("\n");
+
     }
 
     private static List<String> addMembers(List<String> members, String name) {
-        members.add(name);
+        if(!members.contains(name)) {
+            members.add(name);
+        }
+
         return members;
     }
+
+    public static List<String> encryptList(List<String> members) {
+        List<String> encryptedList = new ArrayList<>();
+
+        for(String member : members) {
+            encryptedList.add(encryptName(member));
+        }
+
+        for (int i = 0; i < encryptedList.size(); i++) {
+            if(i % 2 == 0) {
+                encryptedList.set(i, reverseEncryptedName(encryptedList.get(i)));
+            }
+        }
+
+        return encryptedList;
+    }
+
+    private static String encryptName(String name) {
+        StringBuilder encryptedName = new StringBuilder();
+        for (int i = 0; i < name.length(); i++) {
+            String number = charToNumber(name.charAt(i));
+            encryptedName.append(number);
+            if(i < name.length() - 1) {
+                encryptedName.append("-");
+            }
+        }
+        return encryptedName.toString();
+    }
+
+    private static String reverseEncryptedName(String encryptedName) {
+        String[] splitString = encryptedName.split("-");
+        String[] reversedString = new String[splitString.length];
+
+        for (int i = 0; i < splitString.length; i++) {
+            reversedString[splitString.length - 1 - i] = splitString[i];
+        }
+
+        return String.join("-", reversedString);
+    }
+
+    public static String charToNumber(char character){
+        char tempChar = Character.toLowerCase(character);
+        int number = tempChar - 'a' + 1;
+
+        if(Character.isUpperCase(character)) {
+            number = number + 100;
+        }
+
+        return Integer.toString(number);
+    }
+
+
 }
